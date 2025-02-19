0b8fbf97c0b51a133202c1a0d8bded39fbb37739
Erwanlbv <erwan.le-blevec@polytechnique.edu>
2025-02-19 10:14:30 +0100

    corrected sclicing indexes

--- index_list/create_index.py
+++ index_list/create_index.py
@@ -5,19 +5,21 @@ import numpy as np
 ###
 ### Variables to modify
 ###
-save_file_name = "/sps/t2k/eleblevec/updated_watchmal/index_lists/taille_100_000/train55k_val70k_test100k.npz" # you have to include .npz
-nb_events = 99000
+save_file_name = "/sps/t2k/eleblevec/updated_watchmal/WatChMaL/index_list/demo_index/test_new_create_index.npz" # you have to include .npz
 keys = ['train_idxs', 'val_idxs', 'test_idxs'] # ['train_idxs', 'val_idxs', 'test_idxs']
-indexs = [0, 55_000, 70_000, nb_events-1] # [train_first_index, val_first_index, test_first_index, num_events=test_last_index-1]
+
+# indexs[-1] should always be < len(datasets)
+indexs = [0, 10_000, 30_000, 38800] # [train_first_index, val_first_index, test_first_index]
 
 
 ###
 ### No modifications needed below
 ###
-rng = np.random.default_rng()
-a = rng.permutation(range(nb_events))
 
-res = [a[indexs[i]:indexs[i+1]] for i in range(len(indexs) - 1)]
+rng = np.random.default_rng()
+a = rng.permutation(range(indexs[0], indexs[-1]))
+
+res = [a[( indexs[i] - indexs[0] ):( indexs[i+1] - indexs[0] )] for i in range(len(indexs)-1)]
 assert len(keys) == len(res), f"Lenght of keys and res should match. Got {len(keys)} for keys vs {len(res)} for res"
     
 res_dict = {}
